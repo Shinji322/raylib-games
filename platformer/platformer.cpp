8a00a73f09ad0d410a060ed82eb8c9fe3de0090b
Shinji322 <81526911+Shinji322@users.noreply.github.com>
2022-04-14 11:47:23 -0400

    Played around with physics so now it is actually possible

--- platformer/platformer.cpp
+++ platformer/platformer.cpp
@@ -1,5 +1,5 @@
 #include <cstdlib> // for rng 
-#include <iostream>
+#include <iostream> // for console logging
 #include <raylib.h> // for graphical rendering
 
 #define PHYSAC_IMPLEMENTATION
@@ -7,12 +7,14 @@
 #include "helpers.h" // for various helper functions
 
 #define CONSOLE_LOGGING // logs info to console
-// #define CONSOLE_LOGGING_UPDATE // logs info on every update call
+#define TESTING_PLATFORMS // generates platforms on the sides for physics testing
+// #define CONSOLE_LOGGING_UPDATE // logs info on every update call (very messy)
 
 #define MAX_PLATFORMS 5
 #define PHYSICS_ENABLED false
-const Vector2 VELOCITY = (Vector2) { 1.0f, 1.0f };
-const Vector2 ACCELERATION = (Vector2) { 1.0f, 1.0f };
+const Vector2 MAX_VELOCITY = (Vector2) { 1.15f, 4.0f };
+const Vector2 ACCELERATION = (Vector2) { 1.0f, 2.0f };
+const Vector2 GRAVITY = (Vector2) { 0.0f, 9.81f };
 
 //---------------------------------------------------------//
 // Class declarations                                      //
@@ -24,6 +26,7 @@ typedef struct Input
     bool down;
     bool up;
     bool jump;
+    bool refresh;
 } Input;
 // For clarity
 typedef PhysicsBody Platform;
@@ -31,7 +34,11 @@ typedef PhysicsBody Platform;
 class Map
 {
     private:
-        Platform platforms[MAX_PLATFORMS];
+        #ifdef TESTING_PLATFORMS
+            Platform platforms[MAX_PLATFORMS + 2];
+        #else
+            Platform platforms[MAX_PLATFORMS];
+        #endif // TESTING_PLATFORMS
         const int PLATFORM_OFFSET = 20;
 
         void GeneratePlatforms(int screenWidth, int screenHeight)
@@ -39,7 +46,13 @@ class Map
             #ifdef CONSOLE_LOGGING
                 std::cout << "Generating platforms" << std::endl;     
             #endif // CONSOLE_LOGGING
-
+            // float xPos[MAX_PLATFORMS];
+            // float yPos[MAX_PLATFORMS];
+            // for (int i = 0; i < MAX_PLATFORMS; i++)
+            // {
+            //     xPos[i] = std::rand() % (screenWidth - PLATFORM_OFFSET);
+            //     yPos[i] = std::rand() % (screenHeight - PLATFORM_OFFSET);
+            // }
             for (Platform platform : platforms)
             {
                 float x = std::rand() % (screenWidth - PLATFORM_OFFSET);
@@ -59,6 +72,25 @@ class Map
                 platform->useGravity = false;
                 platform->enabled = PHYSICS_ENABLED;
             }
+            #ifdef TESTING_PLATFORMS
+                platforms[MAX_PLATFORMS] = CreatePhysicsBodyRectangle(
+                    (Vector2) { 0, screenHeight - (float)PLATFORM_OFFSET},
+                    50, screenHeight,
+                    1
+                );
+                platforms[MAX_PLATFORMS]->isGrounded = false;
+                platforms[MAX_PLATFORMS]->useGravity = false;
+                platforms[MAX_PLATFORMS]->enabled = PHYSICS_ENABLED;
+
+                platforms[MAX_PLATFORMS + 1] = CreatePhysicsBodyRectangle(
+                    (Vector2) { screenWidth - (float)PLATFORM_OFFSET, screenHeight - (float)PLATFORM_OFFSET},
+                    50, screenHeight,
+                    1
+                );
+                platforms[MAX_PLATFORMS + 1]->isGrounded = false;
+                platforms[MAX_PLATFORMS + 1]->useGravity = false;
+                platforms[MAX_PLATFORMS + 1]->enabled = PHYSICS_ENABLED;
+            #endif // TESTING_PLATFORMS
         }
 
         Platform GenerateGround(int screenWidth, int screenHeight)
@@ -68,8 +100,8 @@ class Map
             #endif // CONSOLE_LOGGING
 
             Vector2 position = (Vector2) {
-                (float)screenWidth - PLATFORM_OFFSET,
-                (float)PLATFORM_OFFSET 
+                (float)screenWidth / 2,
+                screenHeight - (float)PLATFORM_OFFSET 
             };
 
             Platform ground = CreatePhysicsBodyRectangle(
@@ -118,10 +150,9 @@ class Player
                 std::cout << "Initializing player" << std::endl;     
             #endif // CONSOLE_LOGGING
 
-            // TODO investigate why the program is sticking here
             Vector2 position = (Vector2) {
                 groundPosition.x / 2.0f,
-                groundPosition.y + 10,
+                groundPosition.y - 10,
             };
 
             body = CreatePhysicsBodyRectangle(
@@ -133,16 +164,23 @@ class Player
             body->isGrounded = true;
             body->useGravity = true;
             body->freezeOrient = true;
+            body->dynamicFriction = 5;
         }
 
         void Jump()
         {
+            if (body->isGrounded == false) {
+                #ifdef CONSOLE_LOGGING
+                    std::cout << "Player in air. Not jumping" << std::endl;
+                #endif // CONSOLE_LOGGING
+                return;
+            }
+
             #ifdef CONSOLE_LOGGING
                 std::cout << "Jumping" << std::endl;     
             #endif // CONSOLE_LOGGING
 
-            if (body->isGrounded == false) return;
-            body->velocity.y += ACCELERATION.y;
+            body->velocity.y -= ACCELERATION.y;
             body->isGrounded = false;
         }
 
@@ -153,18 +191,26 @@ class Player
             #endif // CONSOLE_LOGGING
 
             if (input.left)
-                body->velocity.x -= ACCELERATION.x;
+            {
+                // body->velocity.x -= ACCELERATION.x;
+                std::cout << "Input left" << std::endl;
+                body->velocity.x = clamp(body->velocity.x - ACCELERATION.x, (-1 * MAX_VELOCITY.x), 0);
+            }
             if (input.right)
-                body->velocity.x += ACCELERATION.x;
+            {
+                // body->velocity.x += ACCELERATION.x;
+                body->velocity.x = clamp(body->velocity.x + ACCELERATION.x, 0, MAX_VELOCITY.x);
+            }
             if (input.down)
             {
                 // TODO Implement a function similar to Smash's fast fall system
-                body->velocity.y -= ACCELERATION.y;
+                if (body->isGrounded == false) 
+                {
+                    body->velocity.y += ACCELERATION.y;
+                }
             }
             if (input.up)
-            {
-                
-            }
+                Jump();
             if (input.jump)
                 Jump();
         }
@@ -184,7 +230,6 @@ class Game
         const int SCREEN_WIDTH = 800;
         const int SCREEN_HEIGHT = 450;
         const int CONFIG_FLAGS = FLAG_MSAA_4X_HINT;
-        const Vector2 GRAVITY = { 0.0f, 9.81f };
         const char *title = "Shinji's Epic Platformer Test"; 
         Camera2D camera = (Camera2D) { 
             (Vector2) {0.0f, 0.0f}, 
@@ -226,6 +271,20 @@ class Game
             );
         }
 
+        void Refresh()
+        {
+            Close();            
+            InitPhysics();
+            SetPhysicsGravity(GRAVITY.x, GRAVITY.y);
+            gameMap.Init(SCREEN_WIDTH, SCREEN_HEIGHT);
+            player.Init(
+                    (Vector2) {
+                        gameMap.ground->position.x,
+                        gameMap.ground->position.y
+                    }
+            );
+        }
+
         Input checkUserInput() 
         {
             #ifdef CONSOLE_LOGGING_UPDATE
@@ -235,18 +294,20 @@ class Game
             // Keyboard
             Input input = (Input) { false, false, false, false, false };
 
-            input.left = IsKeyPressed(KEY_LEFT);
-            input.right = IsKeyPressed(KEY_RIGHT);
+            input.left = IsKeyPressed(KEY_LEFT) || IsKeyDown(KEY_LEFT);
+            input.right = IsKeyPressed(KEY_RIGHT)|| IsKeyDown(KEY_RIGHT);
             input.up = IsKeyPressed(KEY_UP);
             input.down = IsKeyPressed(KEY_DOWN);
             input.jump = IsKeyPressed(KEY_SPACE);
+            input.refresh = IsKeyPressed(KEY_R);
 
-            #ifdef CONSOLE_LOGGING
+            #ifdef CONSOLE_LOGGING_UPDATE
                 std::cout << "input.left = " << input.left << std::endl;
                 std::cout << "input.right = " << input.right << std::endl;
                 std::cout << "input.up = " << input.up << std::endl;
                 std::cout << "input.down = " << input.down << std::endl;
                 std::cout << "input.jump = " << input.jump << std::endl;
+                std::cout << "input.refresh = " << input.refresh << std::endl;
             #endif // CONSOLE_LOGGING
 
             return input;
@@ -269,7 +330,7 @@ class Game
                     for(int j = 0; j < vertices; j++)
                     {
                         Vector2 vertexA = GetPhysicsShapeVertex(body, j);
-                        int k = (((j + 1) < vertices) ? (j + 1) : 0);
+                        int k = (((j + 1) < vertices) ? (j + 1) : 0); // If we're at the last vertex, we need to draw a line to 0
                         Vector2 vertexB = GetPhysicsShapeVertex(body, k);
 
                         DrawLineV(vertexA, vertexB, GREEN);
@@ -292,7 +353,6 @@ class Game
         {
             EndDrawing();
             ClosePhysics();
-            CloseWindow();
         }
 
         Game()
@@ -317,7 +377,10 @@ int main (int argc, char *argv[])
         game.Update();
         game.Draw();
         if (IsKeyPressed(KEY_ESCAPE) || (IsKeyPressed(KEY_Q) && IsKeyPressed(KEY_LEFT_CONTROL)))
+        {
             game.Close();
+            CloseWindow();
+        }
     }
 
     return 0;
