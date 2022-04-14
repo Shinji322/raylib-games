8779cf9b3e336eab771df753627ed19f6ec6d10d
Shinji322 <81526911+Shinji322@users.noreply.github.com>
2022-04-14 11:46:57 -0400

    Added an absolute value function (that I ended up never using)

--- platformer/helpers.cpp
+++ platformer/helpers.cpp
@@ -1,8 +1,20 @@
 #include "helpers.h"
+#include <iostream>
+
+float abs(float value)
+{
+    return (value < 0) ? (value * -1) : (value);
+}
+
+int abs(int value)
+{
+    return (value < 0) ? (value * -1) : (value);
+}
 
 int clamp(int value, int min, int max)
 {
     // Screw it this makes more sense
+    std::cout << "Clamping " << value << ", Min" << min << ", Max" << max << std::endl;
     if (value < min) return min;
     if (value > max) return max;
     return value;
@@ -10,6 +22,7 @@ int clamp(int value, int min, int max)
 
 float clamp(float value, float min, float max)
 {
+    std::cout << "Clamping " << value << ", Min" << min << ", Max" << max << std::endl;
     if (value < min) return min;
     if (value > max) return max;
     return value;
