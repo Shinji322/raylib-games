0e062dbf6d7b120cf0847c7100c7152fce355d91
Shinji322 <81526911+Shinji322@users.noreply.github.com>
2022-04-20 12:37:46 -0400

    Map Draw() implemented

--- platformers/platformer-refactored/map.cpp
+++ platformers/platformer-refactored/map.cpp
@@ -1,1 +1,22 @@
 #include "map.h"
+#include "config.h"
+#include <raylib.h>
+
+void Map::Draw()
+{
+    // Draw Platforms
+    for (Platform platform : platforms) 
+    {
+        DrawTextureV(platformSprite, platform.body->position, LIME);
+    }
+
+    // Draw Walls
+    for (Platform wall : walls)
+    {
+        DrawTextureV(platformSprite, wall.body->position, LIME);
+    }
+
+    // Draw Ground
+    DrawTextureV(groundSprite, ground.body->position, LIME);
+}
+
