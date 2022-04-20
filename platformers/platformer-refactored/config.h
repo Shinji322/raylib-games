114a98f3f62634281e5f043a4905454c8d9f0dd3
Shinji322 <81526911+Shinji322@users.noreply.github.com>
2022-04-20 14:51:48 -0400

    Altered configs a bit

--- platformers/platformer-refactored/config.h
+++ platformers/platformer-refactored/config.h
@@ -7,9 +7,9 @@
 const Vector2 PLAYER_ACCELERATION = (Vector2) { 1.10f, 2.0f };
 const Vector2 MAX_VELOCITY = (Vector2) { 1.15f, 4.0f };
 const Vector2 GRAVITY = (Vector2) { 0.0f, 9.81f };
-const char PLAYER_SPRITE[] = "assets/mario.png";
-const char PLATFORM_SPRITE[] = "assets/platform.jpg";
-const char GROUND_SPRITE[] = "assets/platform.jpg";
+const char PLAYER_SPRITE[] = "assets/Retro-Mario-2-icon.bmp";
+const char PLATFORM_SPRITE[] = "assets/platform.bmp";
+const char GROUND_SPRITE[] = "assets/platform.bmp";
 const int CONFIG_FLAGS = FLAG_MSAA_4X_HINT;
 #define MAX_PLATFORMS 5
 #define PLATFORM_WIDTH 50
@@ -20,4 +20,4 @@ const int CONFIG_FLAGS = FLAG_MSAA_4X_HINT;
 #define WALL_DENSITY 1
 
 
-#endif
+#endif //!CONFIG_H
