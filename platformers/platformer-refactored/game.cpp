5d01f2f1b385436dc791db7b5e051bbad52ddca9
Shinji322 <81526911+Shinji322@users.noreply.github.com>
2022-04-20 12:36:25 -0400

    We are gaming

--- platformers/platformer-refactored/game.cpp
+++ platformers/platformer-refactored/game.cpp
@@ -2,15 +2,57 @@
 #include "physac.h"
 #include "config.h"
 #include "game.h"
+#define PHYSAC_IMPLEMENTATION
 
 void Game::Init()
 {
     InitWindow(
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT,
+        SCREEN_DIMENSIONS.x,
+        SCREEN_DIMENSIONS.y,
         title
     );
 
+    InitPhysics();
     SetPhysicsGravity(GRAVITY.x, GRAVITY.y);
 
+    SetTargetFPS(60);
+
+    map.Init(SCREEN_DIMENSIONS);
+    player.Init((Vector2){
+        SCREEN_DIMENSIONS.x / 2,
+        SCREEN_DIMENSIONS.y - 15
+    });
+    player.SetSprite(PLAYER_SPRITE);
+    player.SetAcceleration(PLAYER_ACCELERATION);
+    player.SetMaxVelocity(MAX_VELOCITY);
+}
+
+void Game::Close()
+{
+    player.Close();
+    map.Close();
+    EndDrawing();
+    ClosePhysics();
+}
+
+void Game::Update()
+{
+    player.Update(inputManager.checkUserInput());
+    map.Update();
+    UpdatePhysics();
+}
+
+void Game::Draw()
+{
+    BeginDrawing();
+        // TODO Implement these methods
+        player.Draw();
+        map.Draw();
+    EndDrawing();
+}
+
+void Game::Loop()
+{
+    Update();
+    Draw();
 }
