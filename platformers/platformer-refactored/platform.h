3166aed08975d396598973eca5f73bf4519c3aa2
Shinji322 <81526911+Shinji322@users.noreply.github.com>
2022-04-20 14:51:31 -0400

    This caused segfaults since I failed to initialize

--- platformers/platformer-refactored/platform.h
+++ platformers/platformer-refactored/platform.h
@@ -2,15 +2,22 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+#include "config.h"
 #include "physac.h"
 
 class Platform 
 {
 public:
-    PhysicsBody body;
+    PhysicsBody body; 
 
     Platform() 
     {
+        body = CreatePhysicsBodyRectangle(
+            (Vector2){ 0, 0 },
+            PLATFORM_WIDTH, PLATFORM_HEIGHT,
+            PLATFORM_DENSITY
+        );
+
         body->isGrounded = false;
         body->useGravity = false;
         body->enabled = false;
@@ -18,4 +25,4 @@ public:
 };
 
 
-#endif // !platform
+#endif // !PLATFROM_H
