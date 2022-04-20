579c35bba6c22343b501168976086741a06baa6e
Shinji322 <81526911+Shinji322@users.noreply.github.com>
2022-04-20 14:50:51 -0400

    I just forgot to call Init like an idiot

--- platformers/platformer-refactored/main.cpp
+++ platformers/platformer-refactored/main.cpp
@@ -1,13 +1,17 @@
+#define PHYSAC_IMPLEMENTATION
 #include "game.h"
 
 int main (int argc, char *argv[])
 {
     Game game;
+
+    game.Init();
     while (!WindowShouldClose())
     {
         game.Loop();
         if (IsKeyDown(KEY_Q))
         {
+            game.Close();
             return 0;
         }
     }
